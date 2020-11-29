788683a26a8550efc1e2d6fd0f8c4f391cfb1255
Zeeshan ‪ <zs.zeeshansaeed22@gmail.com>
2020-11-29 22:46:00 +0500

    Code Update

--- client-http.py
+++ client-http.py
@@ -1,5 +1,7 @@
 # Retrieving Homepage using Python
 
 import requests
-#Vulnerable is pointing to a webpage hosted using apache webserver on Ubuntu VM 
+#Vulnerable is pointing to a webpage hosted using apache webserver on Ubuntu VM
 r =requests.get('http://vulnerable')
+print("Request Status Code {}" r.status_code)
+print("HTML Response {}" r.text)
